f6b0a781747411b2060c1611dce14296cd79bfca
qrlal <pjw322793@gmail.com>
2024-05-31 14:12:17 +0900

    Update ShowScore.cs

--- codes/ShowScore.cs
+++ codes/ShowScore.cs
@@ -1,13 +1,1 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-using UnityEngine.UI;
 
-public class ShowScore : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
-    {
-        GetComponent<Text>().text = "score : "+yes.score.ToString();
-    }
-}
