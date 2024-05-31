c0afdc97e689949f737e3c3417069ebe81ef98ca
qrlal <pjw322793@gmail.com>
2024-05-31 14:12:46 +0900

    Update no.cs

--- codes/no.cs
+++ codes/no.cs
@@ -1,11 +1,1 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
 
-public class no : MonoBehaviour
-{
-    private void OnTriggerEnter2D(Collider2D collision)
-    {
-        Debug.Log("no");
-    }
-}
