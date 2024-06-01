80c10f653691d4e7cd1d8c9eebeb0110711e658b
Hoshiminu <minwoo714211@gmail.com>
2024-06-01 20:40:52 +0900

    Update BirdMove.cs
    플레이어 점프 구현

--- codes/BirdMove.cs
+++ codes/BirdMove.cs
@@ -1,1 +1,20 @@
+using UnityEngine;
 
+public class NewBehaviourScript : MonoBehaviour
+{
+    Rigidbody2D rigid;
+    public float speed = 300f;
+    void Start()
+    {
+        rigid = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            rigid.velocity = Vector2.zero;
+            rigid.AddForce(new Vector2(0, speed));
+        }
+    }
+}
